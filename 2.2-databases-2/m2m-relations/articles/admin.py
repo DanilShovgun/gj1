f4d9ee6d9a25ea7ec714426c257af13d31785d77
Danil Shovgun <shovgun.dan@gmail.com>
2023-09-11 20:33:14 +0500

    modified models, admin, student_list

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_count = sum(form.cleaned_data['is_main'] for form in self.forms if form.cleaned_data)
+        if main_count == 0:
+            raise ValidationError('Должен быть один основной тег.')
+        elif main_count > 1:
+            raise ValidationError('Может быть только один основной тег.')
+        super().clean()
 
 class ScopeInline(admin.TabularInline):
     model = Scope
-    formset = RelationshipInlineFormset
+    formset = ScopeInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     inlines = [ScopeInline]
+
+admin.site.register(Tag)
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,24 +1,27 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    text = models.TextField()
+    image = models.ImageField(upload_to='article_images/')
     tags = models.ManyToManyField(Tag, through='Scope')
+
+    class Meta:
+        ordering = ['title']
 
 class Scope(models.Model):
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
     is_main = models.BooleanField()
-
-class RelationshipInlineFormset(BaseInlineFormSet):
+    
     def clean(self):
-        count_main = 0
-        for form in self.forms:
-            if form.cleaned_data.get('is_main'):
-                count_main += 1
-        if count_main != 1:
-            raise ValidationError('Должен быть указан один и только один основной раздел')
-        return super().clean()
+        main_scopes = Scope.objects.filter(article=self.article, is_main=True)
+        if self.is_main:
+            if main_scopes.exists():
+                raise ValidationError('Основной тег уже существует.')
+        elif not self.is_main and not main_scopes.exists():
+            raise ValidationError('Должен быть один основной тег.')
