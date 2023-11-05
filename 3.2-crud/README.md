9755b6128936c251517e56d25e4a03afa189ae56
Danil Shovgun <shovgun.dan@gmail.com>
2023-11-05 14:06:24 +0500

    Added 3.2-crud/Dockerfile

--- 3.2-crud/README.md
+++ 3.2-crud/README.md
@@ -7,3 +7,6 @@
 Работа должна соответствовать принятому [стилю оформления кода](https://github.com/netology-code/codestyle/tree/master/python).
 
 Любые вопросы по решению задач задавайте в чате учебной группы.
+
+docker build -t my-django-app .
+docker run -p 8000:8000 my-django-app
--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_stocks_products',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
